f97de43d331866c06c99189fbbe4c06ab097dc3a
and <mixmix2@example.com>
2025-09-03 15:24:04 +0300

    task 1

--- AI_house_counter.py
+++ AI_house_counter.py
@@ -1,2 +1,16 @@
 import numpy as np
-import cv2
+import matplotlib.pyplot as plt
+
+
+F = 100
+f0 = 100000
+N0 = 1
+tau = np.linspace(-0.01,0.01,1000)
+R = N0/(np.pi*tau)*np.sin(2*np.pi*F*tau) * np.cos(2*np.pi*f0*tau)
+
+
+plt.plot (tau,R)
+plt.grid(True)
+plt.xlabel("t")
+plt.ylabel("R(t)")
+plt.show()
